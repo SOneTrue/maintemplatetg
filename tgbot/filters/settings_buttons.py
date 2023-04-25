19e6c3b4ffb37acb3321395affdb087d64ec4440
WORK_user <www.pavel_ru.ru@mail.ru>
2023-04-25 21:37:39 +0300

    Настроены кнопки для добавления url адреса в кнопку по желанию.

--- tgbot/filters/settings_buttons.py
+++ tgbot/filters/settings_buttons.py
@@ -7,6 +7,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ReplyKeybo
 class ListOfButtons:
     text: typing.List
     callback: typing.List = None
+    url: typing.List = None
     align: typing.List[int] = None
 
     @property
@@ -23,16 +24,31 @@ def generate_inline_keyboard(args: ListOfButtons) -> InlineKeyboardMarkup:
     if args.text and args.callback and not (len(args.text) == len(args.callback)):
         raise IndexError("All lists must be the same length!")
 
+    if args.url and not (len(args.text) == len(args.url)):
+        raise IndexError("All lists must be the same length!")
+
     if not args.align:
         for num, button in enumerate(args.text):
-            keyboard.add(InlineKeyboardButton(text=str(button),
-                                              callback_data=str(args.callback[num])))
+            if args.url and args.url[num]:
+                keyboard.add(InlineKeyboardButton(text=str(button),
+                                                  url=args.url[num]))
+            else:
+                keyboard.add(InlineKeyboardButton(text=str(button),
+                                                  callback_data=str(args.callback[num])))
     else:
         count = 0
         for row_size in args.align:
-            keyboard.row(*[InlineKeyboardButton(text=str(text), callback_data=str(callback_data))
-                           for text, callback_data in
-                           tuple(zip(args.text, args.callback))[count:count + row_size]])
+            buttons = []
+            if args.url:
+                for text, callback_data, url in tuple(zip(args.text, args.callback, args.url))[count:count + row_size]:
+                    if url:
+                        buttons.append(InlineKeyboardButton(text=str(text), url=url))
+                    else:
+                        buttons.append(InlineKeyboardButton(text=str(text), callback_data=str(callback_data)))
+            else:
+                for text, callback_data in tuple(zip(args.text, args.callback))[count:count + row_size]:
+                    buttons.append(InlineKeyboardButton(text=str(text), callback_data=str(callback_data)))
+            keyboard.row(*buttons)
             count += row_size
     return keyboard
 
--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -1,5 +1,6 @@
 from tgbot.filters.settings_buttons import ListOfButtons
 
-test_keyboard_inl = ListOfButtons(text=['562', '459', '176', '481', '293', '793', '044', '771', '522', '858', '949'],
-                            callback=['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11'],
-                            align=[5, 5, 1]).inline_keyboard
+test_keyboard_inl = ListOfButtons(text=['Перейти на ДО'], callback=['one_call'], align=[1],
+                                  url=['https://moodle.btgp.ru/']).inline_keyboard
+
+test_keyboard_inl_11 = ListOfButtons(text=['Перейти на ДО'], callback=['one_call'], align=[1]).inline_keyboard
